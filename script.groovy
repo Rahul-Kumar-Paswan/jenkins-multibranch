572750ad9cc547cd38c2839209395883760e4db4
Rahul-Kumar-Paswan <rahulkumarpaswan941+auropro@gmail.com>
2023-05-08 10:31:58 +0530

    added script.grovvy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,18 @@
 
 def buildApp(){
-    echo "This is Build stage"
+    echo "This is Build stage in feature branch"
 }
 
 def testApp(){
-    echo "This is test stage"
+    echo "This is test stage in feature branch"
 }
 
 def deployApp(){
-    echo "This is deploy stage"
-    echo "version ${env.VERSION}"
+    echo "This is deploy stage in feature branch"
 }
 
 def prodApp(){
-    echo "This is Production stage"
+    echo "This is Production stage in feature branch"
 }
 
 return this
