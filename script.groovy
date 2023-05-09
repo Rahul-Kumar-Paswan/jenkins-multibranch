609bba8974f923b4f723c384d29291c2cb7c8e38
Rahul-Kumar-Paswan <123447842+Rahul-Kumar-Paswan@users.noreply.github.com>
2023-05-09 10:48:31 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'server-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t rahulkumarpaswan/demo-app:jma-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push rahulkumarpaswan/demo-app:jma-1.0'
